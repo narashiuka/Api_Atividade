50d58b7dfcf86ae32be802441c8b7179c0a608f5
Gabriel Roberto Rodrigues Faria <gabriel.roberto.cep@gmail.com>
2020-06-09 18:23:29 -0300

    Primeira atualização trazendo arquivos recriados a mão

---
+++ api_atividades/models.py
@@ -0,0 +1,37 @@
+
+
+#Bibliotecas necessárias para utilizar o Sqlalchemy
+from sqlalchemy import create_engine, Column, String, Integer, ForeignKey
+from sqlalchemy.orm import scoped_session, sessionmaker, relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+
+#As quatro linhas abaixo são necessárias para se conseguir trabalhar com este banco de dados
+engine = create_engine('sqlite:///atividades.db', convert_unicode=True)
+db_session = scoped_session(sessionmaker(autocommit=False, binds=engine))
+
+Base = declarative_base()
+Base.query = db_session.query_property()
+
+class Pessoas(Base):
+    __tablename__='pessoas'
+    id = Column(Integer, primary_key=True)
+    nome = Column(String(40), index=True)
+    idade = Column(Integer)
+
+    def __repr__(self):
+        return '<Pessoa {}>'.format(self.nome)
+
+class Atividades(Base):
+    __tablename__='atividades'
+    id = Column(Integer, primary_key=True)
+    nome = Column(String(80))
+    pessoa_id = Column(Integer, ForeignKey('pessoas.id'))
+    pessoa = relationship("Pessoas")
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
+
+if __name__ == '__main__':
+    init_db()
+
